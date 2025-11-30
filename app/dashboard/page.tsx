cf6bc329bab24d4116802ae6e9b401bf80315e7b
aadilkhan-uk <aadil.khan6425@gmail.com>
2025-11-30 13:59:46 +0000

    added simple UI setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Dashboard() {
+    return (
+        <div className="min-h-screen bg-background text-foreground p-8 font-sans transition-colors duration-300">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
